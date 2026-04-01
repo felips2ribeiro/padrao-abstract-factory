789ad3c434c704b858f0761995bcaecb93ab14fc
felips2ribeiro <felips2ribeiro@gmail.com>
2026-04-01 09:42:05 -0300

    feat: add Windows concrete products

---
+++ src/main/java/uicomponents/WindowsButton.java
@@ -0,0 +1,8 @@
+package uicomponents;
+
+public class WindowsButton implements IButton {
+    @Override
+    public String render() {
+        return "Renderizando Botão estilo Windows.";
+    }
+}
---
+++ src/main/java/uicomponents/WindowsNotification.java
@@ -0,0 +1,8 @@
+package uicomponents;
+
+public class WindowsNotification implements INotification {
+    @Override
+    public String show() {
+        return "Exibindo Notificação estilo Windows no canto da tela.";
+    }
+}
